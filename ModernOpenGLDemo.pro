5b2a0180bc1573244f5ab3014e56bb75f681633a
Richard Ruzsa <develru@richards-air.speedport.ip>
2014-03-24 22:30:29 +0100

    The glfw library is added to the project, and tested if works.

--- ModernOpenGLDemo.pro
+++ ModernOpenGLDemo.pro
@@ -5,7 +5,8 @@ CONFIG -= qt
 
 SOURCES += main.cpp
 
-LIBS += -L/opt/local/lib -lglfw
+LIBS += -L/opt/local/lib -lglfw -lgl
 
 INCLUDEPATH += /opt/local/include
 
+QMAKE_CXXFLAGS += -std=c++11
--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
-#include <iostream>
+#define GLFW_INCLUDE_GLCOREARB
 
-using namespace std;
+#include <iostream>
+#include <GLFW/glfw3.h>
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+    }
+
+    glfwTerminate();
     return 0;
 }
-
